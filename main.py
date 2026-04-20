6f412900370fbecef576813d6e79fa690f58fd44
GypsyRobot <ramiresero@gmail.com>
2026-04-20 21:15:56 +0100

    feat: add heartbeat

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-from telegram_connector import send_telegram_message, listen_telegram_messages
+import os
+import time
+from dotenv import load_dotenv
+load_dotenv()
 
+from telegram_connector import send_telegram_message, read_telegram_messages
+
+
+heartbeat_interval_seconds = int(os.getenv("HEARTBEAT_INTERVAL_SECONDS", 10))
+now = time.time()
+last_heartbeat = now
+delta_time = 0
+
+def heartbeat() -> bool:
+    global last_heartbeat, delta_time
+    now = time.time()
+    delta_time = now - last_heartbeat
+    if delta_time >= heartbeat_interval_seconds:
+        last_heartbeat = now
+        delta_time = 0
+        return True
+    return False
 
 if __name__ == "__main__":
     send_telegram_message("Ready to work!")
     while True:
-        messages = listen_telegram_messages()
-        for message in messages:
-            print(f"Received message: {message['text']}")
+        time.sleep(1)
+        if heartbeat():
+            messages = read_telegram_messages()
+            if messages:
+                print(f"Received message from Telegram: {messages}")
